fbf000b0e36db8dd502892281f0f30d3bb950fdb
sandor-szabo <21177002+sandor-szabo@users.noreply.github.com>
2023-12-12 22:42:02 +0100

    Update render-redash.sh

--- render-redash.sh
+++ render-redash.sh
@@ -6,4 +6,6 @@ export PYTHONUNBUFFERED=1
 export REDASH_HOST="$RENDER_EXTERNAL_HOSTNAME"
 export REDASH_REDIS_URL="redis://${REDIS_HOSTPORT}/0"
 
+sed -i 's/command=.\/manage.py rq worker %\(ENV_QUEUES\)s/command=.\/manage.py rq worker "%\(ENV_QUEUES\)s"/' /worker.conf
+
 exec /app/bin/docker-entrypoint "$@"
